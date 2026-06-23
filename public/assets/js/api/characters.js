503b04fdfa6dac40b10f5aebc41fa4b0d2c4dd74
Kaique-GM <kaiquegmoficial@gmail.com>
2026-06-23 12:42:36 -0300

    feat: add character list from API and pagination

---
+++ public/assets/js/api/characters.js
@@ -0,0 +1,85 @@
+export async function initCharacters() {
+    let currentUrl = "https://rickandmortyapi.com/api/character";
+    let nextUrl = null;
+    let prevUrl = null;
+
+    const nextBtn = document.getElementById("nextBtn");
+    const prevBtn = document.getElementById("prevBtn");
+
+    const data = await loadCharacters(currentUrl);
+    nextUrl = data.nextUrl;
+    prevUrl = data.prevUrl;
+    updateButtons();
+
+    nextBtn.addEventListener("click", async () => {
+        if (!nextUrl) return;
+
+        const data = await loadCharacters(nextUrl);
+
+        nextUrl = data.nextUrl;
+        prevUrl = data.prevUrl;
+
+        updateButtons();
+    });
+
+    prevBtn.addEventListener("click", async () => {
+        if (!prevUrl) return;
+
+        const data = await loadCharacters(prevUrl);
+
+        nextUrl = data.nextUrl;
+        prevUrl = data.prevUrl;
+
+        updateButtons();
+    });
+
+    function updateButtons() {
+        nextBtn.disabled = !nextUrl;
+        prevBtn.disabled = !prevUrl;
+    }
+}
+
+async function loadCharacters(url) {
+    const container = document.getElementById("cardSection");
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!container) {
+        return;
+    }
+
+    container.innerHTML = "";
+
+    data.results.forEach(character => {
+        const card = document.createElement("div");
+        card.classList.add("cardBox");
+
+        card.innerHTML = `
+            <div class="cardImg">
+                <img src="${character.image}" alt="${character.name}" draggable="false">
+            </div>
+
+            <div class="cardText">
+                <h2>${character.name}</h2>
+
+                <div class="status">
+                    <i class="fa-solid fa-circle"></i>
+                    <span>${character.status} - ${character.species}</span>
+                </div>
+
+                <div class="origem">
+                    <p>Origem: </p>
+                    <span>${character.origin.name}</span>
+                </div>
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
+
+    return {
+        nextUrl: data.info.next,
+        prevUrl: data.info.prev
+    };
+}
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,12 @@
 import { initNavbar } from './modules/navbar.js';
-import { initCards } from './modules/cards.js';
 import { initAnimations } from './modules/animations.js';
+import { initCharacters } from './api/characters.js';
 
 
 document.addEventListener("DOMContentLoaded", () => {
 
     initNavbar();
-    initCards();
     initAnimations();
+    initCharacters();
 
 });

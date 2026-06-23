1b00b59b50e6aefbbfe93726a6b33f6a82b96620
Kaique-GM <kaiquegmoficial@gmail.com>
2026-06-23 10:33:06 -0300

    feat: add static character cards

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,8 @@
 import { initNavbar } from './modules/navbar.js';
+import { initCards } from './modules/cards.js';
 
 document.addEventListener("DOMContentLoaded", () => {
 
     initNavbar();
+    initCards();
 });
